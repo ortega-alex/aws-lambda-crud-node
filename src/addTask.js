5166bd0f3500af445c483adb2773f7af0e561eef
Alex Ortega <mortegalex27@outlook.es>
2023-03-26 23:17:43 -0600

    first commit

---
+++ src/addTask.js
@@ -0,0 +1,39 @@
+const { v4 } = require('uuid')
+const AWS = require('aws-sdk')
+
+const middy = require('@middy/core')
+const jsonBodyParser = require('@middy/http-json-body-parser')
+
+const addTask = async event => {
+    try {
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+        const { title, description } = event.body
+        const createAt = new Date()
+        const id = v4()
+
+        const newTask = {
+            id,
+            title,
+            description,
+            createAt,
+            done: false
+        }
+
+        await dynamodb.put({
+            TableName: 'TaskTable',
+            Item: newTask
+        }).promise()
+
+        return {
+            status: 200,
+            body: JSON.stringify(newTask)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    addTask: middy(addTask).use(jsonBodyParser())
+}
---
+++ src/hello.js
@@ -0,0 +1,14 @@
+module.exports.hello = async (event) => {
+  // code goes here
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello world",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
